2006747cba30c9f97491775fa3b424a489ce071c
Dustin Myers <dustint314@gmail.com>
2021-02-23 11:57:01 -0700

    wip

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -8,14 +8,14 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 
-function PrivateRoute({ component, ...rest }) {
+function PrivateRoute({ component: Component, ...rest }) {
   return (
     <Route
       {...rest}
       render={() => {
         if (localStorage.getItem("token")) {
           // render component
-          return <component />;
+          return <Component />;
         } else {
           // route to login
           return <Redirect to="/login" />;
