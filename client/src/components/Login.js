7dfd4a65a2e4b9104e308d32492531f9054f0072
Dustin Myers <dustint314@gmail.com>
2021-02-23 10:21:15 -0700

    wip

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 class Login extends React.Component {
   state = {
     credentials: {
-      username: '',
-      password: ''
-    }
+      username: "",
+      password: "",
+    },
   };
 
-  handleChange = e => {
+  handleChange = (e) => {
     this.setState({
       credentials: {
         ...this.state.credentials,
-        [e.target.name]: e.target.value
-      }
+        [e.target.name]: e.target.value,
+      },
     });
   };
 
-  login = e => {
+  login = (e) => {
     e.preventDefault();
+    // axios call to login
+    // we will get a token back
+    // store the token in localStorage
   };
 
   render() {
