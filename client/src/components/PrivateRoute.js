6cb037286a270d691d31ebf24e06dab6e763687d
Dustin Myers <dustint314@gmail.com>
2021-02-23 11:45:34 -0700

    wip

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,6 +1,6 @@
 // replace the "Route" component in our Routing
-// PrivateRoute should be able to take in all the same props as `<Route />`
-// Render a <Route /> and foward the given props to it
+// PrivateRoute should be able to take in all the same props as `<Route />` ✅
+// Render a <Route /> and foward the given props to it ✅
 // should check to see if user is authenticated
 // if yes, render the given comp
 // if not, re-route to the login page
@@ -9,7 +9,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 function PrivateRoute(props) {
-  return <Route {...props} />;
+  return <Route {...props} render={() => {}} />;
 }
 
 export default PrivateRoute;
